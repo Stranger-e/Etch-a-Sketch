55db82ed897de969775f3922864b70e0cd7915d3
Ezekiel OLoo <olooezekiel885@gmail.com>
2024-01-25 15:05:28 +0300

    Add a user selection and a dark theme

--- script.js
+++ script.js
@@ -2,11 +2,24 @@ const GRIDAREA = 600
 
 let squares = 30
 
+let sliderContainer = document.querySelector('.slider-container')
+let slider = document.querySelector('.slider')
+let inputValue = document.querySelector('.inputvalue')
+let blackTheme = document.querySelector('.black')
+let body = document.querySelector('body')
+let head = document.querySelector('h1')
+
+blackTheme.addEventListener('click', function() {
+    body.style.backgroundColor = 'black'
+    inputValue.style.color = 'white'
+    head.style.color = 'white'
+})
+
 let gridContainer = document.querySelector('.gridcontainer')
 gridContainer.style.width = `${GRIDAREA}px`
 gridContainer.style.height = `${GRIDAREA}px`
 
-function createGrid() {
+function createGrid(squares) {
 
     for (let i = 0; i < squares * squares; i++){
         let gridItem = document.createElement('div')
@@ -21,5 +34,19 @@ function createGrid() {
         })
     }
 }
+
+function removeGridItem(){
+    while (gridContainer.firstChild){
+        gridContainer.removeChild(gridContainer.firstChild)
+    }
+}
+
+slider.oninput = function(){
+    removeGridItem()
+    createGrid(this.value)
+    inputValue.textContent = `${this.value} X ${this.value} Sauares`
+}
+
+
 
 createGrid()
